62d0f9736e4e2ca87b71ce5551c5dafd0758409f
Abraham Mamani <abraham.e.mamani@gmail.com>
2022-11-06 11:45:07 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Message } from '@components/index'
+import { MessengerView } from '@views/MessengerView'
+
+function App() {
+  return (
+    <div className="app">
+      {/* <MessengerView /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/MessengerView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MessengerView = () => {
+  return (
+    <div>MessengerView</div>
+  )
+}
